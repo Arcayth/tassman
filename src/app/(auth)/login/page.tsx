4ad112c7ee8909ed2a49b36ca14c1549c7c8f82a
Arcayth <h.chanouni4444@uca.ac.ma>
2024-07-22 11:22:28 +0100

    chore: update eslint configuration to disable react/no-unescaped-entities rule

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -29,7 +29,7 @@ const Signup = () => {
         </CardContent>
         <CardFooter className="text-center">
           <p>
-            Don&apos;t have an account?{" "}
+            Don't have an account?{" "}
             <a href="/signup" className="underline">
               Sign Up 
             </a>
